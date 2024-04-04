be61c6540375f569bf50deccd9551c9be39d459b
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-04-04 15:17:54 +0700

    membuat minimum dan maximum pada elemen arrau

--- pertemuan ketujuh algoritman search/pertemuan ketujuh algoritman search.cpp
+++ pertemuan ketujuh algoritman search/pertemuan ketujuh algoritman search.cpp
@@ -13,6 +13,10 @@ void input ()
     {
         cout << "Enter the number of element in the array: ";
         cin >> n;
+        if ((n > 0) && (n <=20))
+            break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 elements. \n\n";
     }
 }
 {
