0b3657a3db67e430676db539b48e1e1201b14b53
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-04-04 15:10:24 +0700

    membuat output untuk memasukkan elemen dalam array

--- pertemuan ketujuh algoritman search/pertemuan ketujuh algoritman search.cpp
+++ pertemuan ketujuh algoritman search/pertemuan ketujuh algoritman search.cpp
@@ -6,6 +6,15 @@ using namespace std;
 int arr[20]; // array to be searched
 int n;      // number of elements in the array
 int i;      // index of array element
+
+void input ()
+{
+    while (true)
+    {
+        cout << "Enter the number of element in the array: ";
+        
+    }
+}
 {
     std::cout << "Hello World!\n";
 }
