d554bf9e22422cf2d28fa20df7321e35b9b7c560
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-04-04 15:22:42 +0700

    membuat ouput untuk memasukkan elemen array

--- pertemuan ketujuh algoritman search/pertemuan ketujuh algoritman search.cpp
+++ pertemuan ketujuh algoritman search/pertemuan ketujuh algoritman search.cpp
@@ -18,6 +18,10 @@ void input ()
         else
             cout << "\nArray should have minimum 1 and maximum 20 elements. \n\n";
     }
+    // Accept array elements
+    cout << "\n-------------------\n";
+    cout << " Enter array elements \n";
+    cout << "----------------------\n";
 }
 {
     std::cout << "Hello World!\n";
