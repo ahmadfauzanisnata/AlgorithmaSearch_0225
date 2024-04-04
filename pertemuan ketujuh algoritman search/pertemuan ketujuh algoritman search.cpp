0289f858d05df8b9f0bce4fa2357a8e025b30266
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-04-04 15:55:29 +0700

    membuat step 4

--- pertemuan ketujuh algoritman search/pertemuan ketujuh algoritman search.cpp
+++ pertemuan ketujuh algoritman search/pertemuan ketujuh algoritman search.cpp
@@ -49,7 +49,9 @@ void LinearSearch()
             if (arr[i] == item)
             {
                 cout << "\n" << item << " found at position " << (i +1) << endl; // step 6
+                break;
             }
+            i++; // step 4
         }
 }
 {
